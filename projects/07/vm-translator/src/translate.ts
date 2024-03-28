9a1abe5025cb6fdbf1de4ac7ac8cc1c47989600f
김명재 Myeongjae Kim <dev@myeongjae.kim>
2024-03-28 23:22:30 +0900

    [#4] implement and

--- projects/07/vm-translator/src/translate.ts
+++ projects/07/vm-translator/src/translate.ts
@@ -255,6 +255,31 @@ M=M+1`;
       return asm;
     }
 
+    if (code === 'and') {
+      return `// and
+// pop to D
+@SP
+M=M-1
+A=M
+D=M
+
+// pop to A
+@SP
+M=M-1
+A=M
+
+// and
+D=M&D
+
+// push
+@SP
+A=M
+M=D
+// SP++
+@SP
+M=M+1`;
+    }
+
     throw new Error('Not implemented');
   };
 
--- projects/07/vm-translator/test/translate.unit.test.ts
+++ projects/07/vm-translator/test/translate.unit.test.ts
@@ -288,4 +288,31 @@ describe('translate', () => {
    M=M+1`.trim(),
     );
   });
+
+  it('should handle "and"', () => {
+    expect(translate('and', translateDefaultOptions())).toEqual(
+      `// and
+// pop to D
+   @SP
+   M=M-1
+   A=M
+   D=M
+
+// pop to A
+   @SP
+   M=M-1
+   A=M
+
+// and
+   D=M&D
+
+// push
+   @SP
+   A=M
+   M=D
+// SP++
+   @SP
+   M=M+1`.trim(),
+    );
+  });
 });
